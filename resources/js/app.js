2ee648cb71c53c7754575975096ab593b962163b
sunpengfei <spf_roc@163.com>
2021-03-31 15:18:28 +0800

    fix errors

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,5 @@
 require('./bootstrap');
 
-import Vue from 'vue';
-window.Vue = require('vue');
-
-import App from './App.vue';
-import VueRouter from 'vue-router';
-import VueAxios from 'vue-axios';
-import axios from 'axios';
-import {routes} from './routes';
-
-Vue.use(VueRouter);
-Vue.use(VueAxios, axios);
-
-const router = new VueRouter({
-    mode: 'history',
-    routes: routes
-});
-
-const app = new Vue({
-    el: '#app',
-    router: router,
-    render: h => h(App),
-});
-
-console.log(111111111111111);
-Echo.join(`test.channel`)
-
-
 Echo.channel('my-channel-1')
     .listen('my-event', (e) => {
         console.log(e);
